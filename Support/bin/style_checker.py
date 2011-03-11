59979bc02ccc61dcb5af723d036cc3000d5c8425
Chris McKenzie <me@christophermckenzie.com>
2011-03-11 00:54:27 -0500

    added style checker

---
+++ Support/bin/style_checker.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from settings import *
+import os
+
+try:
+    from closure_linter import checker
+    from closure_linter import error_fixer
+    from closure_linter.common import simplefileflags as fileflags
+except:
+    raise Exception("Please install the closure linter first")
+
+style_checker = checker.JavaScriptStyleChecker(error_fixer.ErrorFixer())
+style_checker.Check(os.environ['TM_FILEPATH'])
